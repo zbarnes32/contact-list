0dd05b13df7b994db8f2cdbcb683cd55d8d9496d
Jake Overall <joverall22@gmail.com>
2022-04-11 16:32:22 -0600

    Update main.js

--- main.js
+++ main.js
@@ -10,18 +10,6 @@ let contacts = []
  * *** push: resources/push.jpg
  */
 function addContact(event) {
-  event.preventDefault()
-  let form = event.target
-
-  let contact = {
-    id: generateId(),
-    name: form.name.value,
-    phone: form.phone.value,
-    emergencyContact: form.emergencyContact.checked
-  }
-  contacts.push(contact)
-  saveContacts()
-  form.reset()
 }
 
 /**
@@ -29,8 +17,6 @@ function addContact(event) {
  * Saves the string to localstorage at the key contacts 
  */
 function saveContacts() {
-  window.localStorage.setItem("contacts", JSON.stringify(contacts))
-  drawContacts()
 }
 
 /**
@@ -39,10 +25,6 @@ function saveContacts() {
  * the contacts array to the retrieved array
  */
 function loadContacts() {
-  let storedContacts = JSON.parse(window.localStorage.getItem("contacts"))
-  if (storedContacts) {
-    contacts = storedContacts
-  }
 }
 
 /**
@@ -51,23 +33,6 @@ function loadContacts() {
  * contacts in the contacts array
  */
 function drawContacts() {
-  let contactListElement = document.getElementById("contact-list")
-  let contactsTemplate = ""
-  contacts.forEach(contact => {
-    contactsTemplate += `
-    <div class="contact-card card mt-1 mb-1 ${contact.emergencyContact ? 'emergency-contact' : ''}">
-      <h3 class="mt-1 mb-1">${contact.name}</h3>
-      <div class="d-flex space-between">
-          <p>
-            <i class="fa fa-fw fa-phone"></i>
-            <span>${contact.phone}</span>
-          </p>
-          <i class="action fa fa-trash text-danger" onclick="removeContact('${contact.id}')"></i>
-        </div>
-    </div>
-    `
-  })
-  contactListElement.innerHTML = contactsTemplate
 }
 
 /**
@@ -80,19 +45,12 @@ function drawContacts() {
  * @param {string} contactId 
  */
 function removeContact(contactId) {
-  let index = contacts.findIndex(contact => contact.id == contactId)
-  if (index == -1) {
-    throw new Error("Invalid Contact Id")
-  }
-  contacts.splice(index, 1)
-  saveContacts()
 }
 
 /**
  * Toggles the visibility of the AddContact Form
  */
 function toggleAddContactForm() {
-  document.getElementById('new-contact-form').classList.toggle("hidden")
 }
 
 
